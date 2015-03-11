c9773efa62cf0961080024bf874e51741eb30942
Matthieu Louchet <matt.louc@gmail.com>
2015-03-11 21:18:22 +0100

    1 to 4 players gestion + card done only once.

--- src/esgi/blackjack/bean/CardPack.java
+++ src/esgi/blackjack/bean/CardPack.java
@@ -1,10 +1,14 @@
 package esgi.blackjack.bean;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+import esgi.blackjack.utils.Utils;
+
 public class CardPack {
-	public List<Card> deck;
+	private List<Card> deck;
+	private List<Integer> indexesDone = new ArrayList<Integer>();
 	
 	public CardPack() {
 		// TODO Auto-generated constructor stub
@@ -12,5 +16,22 @@ public class CardPack {
 		for(int y=1;y<5;y++)
 		for(int i=1;i<14;i++)
 			deck.add(new Card(i,y));
+	}
+	
+	public Card getCard() {
+		
+		if(indexesDone.size() == 52)
+			return null;
+		
+		int index = 0;
+		
+		do {
+			index = Utils.randCard();
+		} while(indexesDone.contains(index));
+		
+		indexesDone.add(index);
+			
+		return deck.get(index);
+		
 	}
 }
--- src/esgi/blackjack/windows/MainWindow.java
+++ src/esgi/blackjack/windows/MainWindow.java
@@ -1,18 +1,18 @@
 package esgi.blackjack.windows;
 	
-import java.awt.Button;
 import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import esgi.blackjack.bean.Card;
@@ -20,18 +20,27 @@ import esgi.blackjack.bean.CardPack;
 import esgi.blackjack.bean.Player;
 import esgi.blackjack.listener.FinishListener;
 import esgi.blackjack.utils.ImageBankPanel;
-import esgi.blackjack.utils.Utils;
 
+@SuppressWarnings("serial")
 public class MainWindow extends JFrame{
 	JPanel bankPanel, bankCardPanel, playerPanel;
+	
 	List<JPanel> listPlayerPanel;
+	List<Player> players = new ArrayList<Player>();
+	List<JButton> askForCardButtons = new ArrayList<JButton>();
+	List<JButton> finishButtons = new ArrayList<JButton>();
+	
 	CardPack cardPack;
-	JButton askForCardButton;
-	JButton finishButton;
 	JButton leaveTableButton;
-	Player player;
+	
+	private int nbPlayer = 4;
 
 	public MainWindow(){
+		if(nbPlayer > 4)
+			nbPlayer = 4;
+		else if(nbPlayer < 0)
+			nbPlayer = 1;
+		
 		initUI();
 		initZones();
 		initPlayerPanel();
@@ -52,7 +61,7 @@ public class MainWindow extends JFrame{
 		playerPanel = new JPanel();
 		listPlayerPanel = new LinkedList<JPanel>();
 		
-		for(int i=0;i<4;i++){
+		for(int i=0;i<nbPlayer;i++){
 			JPanel aPanel = new JPanel();
 			if((i%2)==0)aPanel.setBackground(Color.red);else aPanel.setBackground(Color.yellow);
 			aPanel.setLayout(new GridBagLayout());
@@ -62,7 +71,7 @@ public class MainWindow extends JFrame{
 		bankPanel.setBackground(Color.green);
 		bankPanel.setLayout(new GridLayout(0,3));
 		playerPanel.setBackground(Color.black);
-		playerPanel.setLayout(new GridLayout(0,4));
+		playerPanel.setLayout(new GridLayout(0,nbPlayer));
 		
 		for (JPanel aPanel : listPlayerPanel) {
 			playerPanel.add(aPanel);
@@ -72,31 +81,40 @@ public class MainWindow extends JFrame{
 	}
 	
 	private void initPlayerPanel(){
-		player = new Player();
-		askForCardButton = new JButton("Prendre une carte");
-		finishButton = new JButton("Terminer");
+		askForCardButtons.clear();
+		finishButtons.clear();
+		players.clear();
 		
-		askForCardButton.addActionListener(new ActionListener() {
+		for(int i = 0 ; i < this.listPlayerPanel.size() ; ++i) {
+			players.add(new Player());
+
+			JButton currentCardButton = new JButton("Prendre une carte");
+			JButton currentFinish = new JButton("Terminer");
+						
+			currentCardButton.addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					addCard(e.getSource());
+				}
+			});
+			currentFinish.addActionListener(new FinishListener());
 			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				addCard();
-			}
-		});
-		finishButton.addActionListener(new FinishListener());
-		
-		GridBagConstraints gbc = new GridBagConstraints();
-		gbc.gridx=0;
-		gbc.gridy=0;
-		gbc.weightx = gbc.weighty = 1;
-		gbc.fill=GridBagConstraints.BOTH;
-		gbc.gridheight=1;
-		
-		listPlayerPanel.get(0).add(askForCardButton,gbc);
-		
-		gbc.gridx=1;
-		listPlayerPanel.get(0).add(finishButton,gbc);
+			GridBagConstraints gbc = new GridBagConstraints();
+			gbc.gridx=0;
+			gbc.gridy=0;
+			gbc.weightx = gbc.weighty = 1;
+			gbc.fill=GridBagConstraints.BOTH;
+			gbc.gridheight=1;
+			
+			listPlayerPanel.get(i).add(currentCardButton,gbc);
+			
+			gbc.gridx=1;
+			listPlayerPanel.get(i).add(currentFinish,gbc);
+
+			askForCardButtons.add(currentCardButton);
+			finishButtons.add(currentFinish);
+		}
 	}
 	
 	private void initBankPanel(){
@@ -119,30 +137,43 @@ public class MainWindow extends JFrame{
 	
 	private void distribute(){
 		cardPack = new CardPack();
-		Card firstCard = cardPack.deck.get(Utils.randCard());
-		Card secondCard = cardPack.deck.get(Utils.randCard());
-		player.addCard(firstCard.cardNumber);
-		player.addCard(secondCard.cardNumber);
-		System.out.println(player.score);
 		
-		GridBagConstraints gbc = new GridBagConstraints();
-		gbc.gridx=0;
-		gbc.gridy=1;
-		gbc.fill=GridBagConstraints.BOTH;
-		gbc.weightx = 1;
-		gbc.weighty = 40;
+		for(int i = 0 ; i < this.players.size() ; ++i) {
+			
+			Card firstCard = cardPack.getCard();
+			Card secondCard = cardPack.getCard();
 		
-		listPlayerPanel.get(0).add(firstCard,gbc);
+			players.get(i).addCard(firstCard.cardNumber);
+			players.get(i).addCard(secondCard.cardNumber);
+			System.out.println(players.get(i).score);
 		
-		gbc.gridx=1;
-		listPlayerPanel.get(0).add(secondCard,gbc);
+			GridBagConstraints gbc = new GridBagConstraints();
+			gbc.gridx=0;
+			gbc.gridy=1;
+			gbc.fill=GridBagConstraints.BOTH;
+			gbc.weightx = 1;
+			gbc.weighty = 40;
+			
+			listPlayerPanel.get(i).add(firstCard,gbc);
+			
+			gbc.gridx=1;
+			listPlayerPanel.get(i).add(secondCard,gbc);
+		}
 
 	}
 	
-	private void addCard(){
-		Card aCard = cardPack.deck.get(Utils.randCard());
-		player.addCard(aCard.cardNumber);
-		System.out.println(player.score);
+	private void addCard(Object source){
+		
+		Card aCard = cardPack.getCard();
+		
+		if(aCard == null) {
+			JOptionPane.showMessageDialog(null,"Plus de carte dans le deck !");
+		}
+		
+		int indexPlayer = this.askForCardButtons.indexOf(source);
+		
+		players.get(indexPlayer).addCard(aCard.cardNumber);
+		System.out.println(players.get(indexPlayer).score);
 		
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.gridx = 0;
@@ -150,10 +181,10 @@ public class MainWindow extends JFrame{
 		gbc.fill=GridBagConstraints.BOTH;
 		gbc.weightx = 1;
 		gbc.weighty = 40;
-		listPlayerPanel.get(0).remove(2);
-		listPlayerPanel.get(0).add(listPlayerPanel.get(0).getComponent(2),gbc);
+		listPlayerPanel.get(indexPlayer).remove(2);
+		listPlayerPanel.get(indexPlayer).add(listPlayerPanel.get(indexPlayer).getComponent(2),gbc);
 		gbc.gridx = 1;
-		listPlayerPanel.get(0).add(aCard,gbc);
+		listPlayerPanel.get(indexPlayer).add(aCard,gbc);
 		validate();
 	}
 }
